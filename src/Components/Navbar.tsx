40387c0a4922a1c2ec54463b65b741834839f5fd
Priyanshu T <priyanshu9943@gmail.com>
2023-02-27 21:58:43 +0530

    Version 1.0.0 Uploaded

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -6,20 +6,23 @@ import { Link } from 'react-router-dom'
 
 const Navbar: React.FC<{}> = () => {
   return (
-    <nav className="p-4 flex shadow items-center">
+    <nav className="p-4 flex items-center w-full absolute">
       <span className="font-semibold text-lg">EWhizard</span>
       <ul className="flex ml-auto space-x-3">
         <li>
           <Link to={'/'}>Home</Link>
         </li>
         <li>
-          <Link to={'/gallery'}>Gallery</Link>
+          <Link to={'/#gallery'}>Gallery</Link>
         </li>
         <li>
-          <Link to={'/services'}>Services</Link>
+          <Link to={'/#services'}>Services</Link>
         </li>
         <li>
-          <Link to={'/contacts'}>Contacts</Link>
+          <Link to={'/#contacts'}>Contacts</Link>
+        </li>
+        <li>
+          <Link to={'/#community'}>Community</Link>
         </li>
       </ul>
     </nav>
